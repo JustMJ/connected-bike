50a12093fb2329d284068c0e79cdda4a053b9206
JustMJ <justinmjones23@gmail.com>
2026-03-12 17:47:56 -0400

    Updates

---
+++ precache-manifest.e3c73f5b69eb0d59e601ab86995cf470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf060eefc1ee92238de9e033e6c35f8",
+    "url": "/connected-bike/index.html"
+  },
+  {
+    "revision": "a8fd2b1e3c2e80834eb6",
+    "url": "/connected-bike/static/css/main.732e7323.chunk.css"
+  },
+  {
+    "revision": "98ece5a08b7db68b362f",
+    "url": "/connected-bike/static/js/2.a112baac.chunk.js"
+  },
+  {
+    "revision": "1d8dfa16d11c0ca8f44c5b5214fef4d3",
+    "url": "/connected-bike/static/js/2.a112baac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8fd2b1e3c2e80834eb6",
+    "url": "/connected-bike/static/js/main.34dbfab8.chunk.js"
+  },
+  {
+    "revision": "e79c6cfbab9350e2706f",
+    "url": "/connected-bike/static/js/runtime-main.d81d212f.js"
+  }
+]);
